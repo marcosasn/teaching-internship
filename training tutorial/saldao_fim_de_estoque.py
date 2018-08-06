a31501a26a5208c38740a6854eb34343d0abed88
marcosasn <marcosnascimennto@gmail.com>
2018-08-06 10:14:42 -0300

    Updating a assignmment soluction.

--- training tutorial/saldao_fim_de_estoque.py
+++ training tutorial/saldao_fim_de_estoque.py
@@ -1,20 +1,21 @@
-#coding: utf-8
-#Marcos Nascimento, 2018
+# coding: utf-8
+# Questão: Saldao fim de estoque
+# (C) 2018, Marcos Nascimento / UFCG, Teaching internship
 
 FIM = "fim"
-MINIMO_DESCONTO = 50.0
-cont_produtos = 0
+PRECO_DESCONTO = 50.0
+produtos = 0
 preco_total, preco_desconto = 0.0, 0.0
 
 while True:
-	entrada = raw_input()
-	if entrada.lower() == FIM:
+	entrada = raw_input().lower()
+	if entrada == FIM:
 		break
 	else:
 		produto, preco_produto = entrada.split(" ")
 		if produto:
-			cont_produtos += 1
-		if float(preco_produto) > MINIMO_DESCONTO:
+			produtos += 1
+		if float(preco_produto) > PRECO_DESCONTO:
 			preco_total += float(preco_produto)
 			preco_produto = (float(preco_produto) - (float(preco_produto)*.4))
 			preco_desconto += preco_produto
@@ -22,6 +23,6 @@ while True:
 			preco_total += float(preco_produto)
 			preco_desconto += float(preco_produto)
 
-print "Total de produtos: {}".format(cont_produtos)
+print "Total de produtos: {}".format(produtos)
 print "Preço total: R$ {:.2f}".format(preco_total)
 print "Total com desconto: R$ {:.2f}".format(preco_desconto)
