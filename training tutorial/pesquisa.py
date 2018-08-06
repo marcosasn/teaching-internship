cba04406b05dfb056c7cfa2cdc12814091e26502
marcosasn <marcosnascimennto@gmail.com>
2018-08-06 09:26:19 -0300

    Updating a assignmment soluction.

--- training tutorial/pesquisa.py
+++ training tutorial/pesquisa.py
@@ -1,13 +1,17 @@
-#coding: utf-8
-#Marcos Nascimento, 2018
+# coding: utf-8
+# Questão: Trabalho de pesquisa
+# (C) 2018, Marcos Nascimento / UFCG, Teaching internship
 
 todas_infos = dict()
-conta_contra, conta_afavor, conta_neutro = 0, 0, 0
+FIM = "fim"
+CONTRA = "Contra"
+AFAVOR = "A favor"
+conta_contra, conta_afavor, conta_neutro = 0.0, 0.0, 0.0
 
 def ordenar_infos(todos_nomes):
-    for passnum in range(len(todos_nomes)-1,0,-1):
-        for i in range(passnum):
-            if todos_nomes[i]>todos_nomes[i+1]:
+    for j in range(len(todos_nomes)-1,0,-1):
+        for i in range(j):
+            if todos_nomes[i] > todos_nomes[i+1]:
                 temp = todos_nomes[i]
                 todos_nomes[i] = todos_nomes[i+1]
                 todos_nomes[i+1] = temp
@@ -15,7 +19,7 @@ def ordenar_infos(todos_nomes):
 				 
 while True:
 	nome = raw_input() 
-	if nome == 'fim':
+	if nome == FIM:
 		break
 	else:
 		telefone = raw_input()
@@ -30,21 +34,21 @@ if len(todas_infos.keys()) > 0:
 		print("E-mail: %s" % todas_infos[nome][0])
 		print("Voto: %s" % todas_infos[nome][2])
 		print("----------------------")
-		if todas_infos[nome][2] == "Contra":
-			conta_contra += 1.0
-		elif todas_infos[nome][2] == "A favor":
-			conta_afavor += 1.0
+		if todas_infos[nome][2] == CONTRA:
+			conta_contra += 1
+		elif todas_infos[nome][2] == AFAVOR:
+			conta_afavor += 1
 		else:
-			conta_neutro += 1.0
+			conta_neutro += 1
 
 	print "Estatísticas da Pesquisa"
 	print "----------------------"
-	print "{} Participante(s) votaram a favor, significando ({:5.2f}%) dos votos.".format(int(conta_afavor), (conta_afavor/len(todas_infos.keys())*100))
-	print "{} Participante(s) votaram contra, significando ({:5.2f}%) dos votos.".format(int(conta_contra), (conta_contra/len(todas_infos.keys())*100))
-	print "{} Participante(s) votaram neutro, significando ({:5.2f}%) dos votos.".format(int(conta_neutro), (conta_neutro/len(todas_infos.keys())*100))
+	print "{} Participante(s) votaram a favor, significando ({:.2f}%) dos votos.".format(int(conta_afavor), (conta_afavor/len(todas_infos.keys())*100))
+	print "{} Participante(s) votaram contra, significando ({:.2f}%) dos votos.".format(int(conta_contra), (conta_contra/len(todas_infos.keys())*100))
+	print "{} Participante(s) votaram neutro, significando ({:.2f}%) dos votos.".format(int(conta_neutro), (conta_neutro/len(todas_infos.keys())*100))
 else:
 	print "Estatísticas da Pesquisa"
 	print "----------------------"
-	print "{} Participante(s) votaram a favor, significando ({:5.2f}%) dos votos.".format(int(conta_afavor), conta_afavor)
-	print "{} Participante(s) votaram contra, significando ({:5.2f}%) dos votos.".format(int(conta_contra), conta_contra)
-	print "{} Participante(s) votaram neutro, significando ({:5.2f}%) dos votos.".format(int(conta_neutro), conta_neutro)
+	print "{} Participante(s) votaram a favor, significando ({:.2f}%) dos votos.".format(int(conta_afavor), conta_afavor)
+	print "{} Participante(s) votaram contra, significando ({:.2f}%) dos votos.".format(int(conta_contra), conta_contra)
+	print "{} Participante(s) votaram neutro, significando ({:.2f}%) dos votos.".format(int(conta_neutro), conta_neutro)
